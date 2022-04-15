413edb8de241c9ac3e29229837dbb288803694d7
Galvier <jonathangalvier@gmail.com>
2022-04-15 16:42:29 -0300

    Subindo pasta Java para Git

---
+++ JavaScript/helloworld.js
@@ -0,0 +1,14 @@
+var funcao = "Hello World 2";
+function imprimirfuncao(funcaoParaImprimir) {
+  console.log(funcaoParaImprimir);
+}
+
+imprimirfuncao(funcao);
+
+var letras = ["A", "B", "C"];
+console.log(letras[2]);
+
+var linguagens = ["JavaScript", "PHP", "JAVA", "C#"];
+
+console.log(linguagens[0]); //resultado impresso: JavaScript
+console.log(linguagens[3]); //resultado impresso: C#console.log(letras[1]);
